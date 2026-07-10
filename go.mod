5d1db768c8b6a455169a8849075c9647012fa876
Tristan Lavocat <107067429+Tritounet1@users.noreply.github.com>
2026-07-10 13:49:09 +0200

    feat: add proxy single target

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module tidy
 
 go 1.24.1
+
+tool github.com/joho/godotenv/cmd/godotenv
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+func health(w http.ResponseWriter, req *http.Request) {
+	// TODO: retourner un json propre
+	fmt.Fprintf(w, "ok\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
@@ -17,11 +25,54 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func redirectToTls(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://IPAddr:443"+r.RequestURI, http.StatusMovedPermanently)
+}
+
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	fmt.Println("Server starting on port 8090")
+	debug, err := strconv.ParseBool(os.Getenv("DEBUG"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	http.ListenAndServe(":8090", nil)
+	mux := http.NewServeMux()
+
+	// curl -v -H "Host: tritounet.fr" http://localhost
+	target := url.URL{Scheme: "https", Host: "tritounet.fr", Path: "/"}
+
+	proxy := &httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			if debug {
+				dump, err := httputil.DumpRequest(r.In, true)
+				if err != nil {
+					log.Fatal("Error while trying to dump the request : ", err.Error())
+				}
+				fmt.Printf("Dump IN : %s\n", dump)
+			}
+			r.SetURL(&target)
+			r.SetXForwarded()
+			if debug {
+				dump, err := httputil.DumpRequestOut(r.Out, true)
+				if err != nil {
+					log.Fatal("Error while trying to dump the request : ", err.Error())
+				}
+				fmt.Printf("Dump OUT : %s\n", dump)
+			}
+		},
+	}
+
+	mux.HandleFunc("/health", health)
+	mux.HandleFunc("/headers", headers)
+	mux.Handle("/", proxy)
+
+	fmt.Println("Server starting on port 80")
+
+	if err := http.ListenAndServe(":80", mux); err != nil {
+		log.Fatalf("ListenAndServe error: %v", err)
+	}
 }
